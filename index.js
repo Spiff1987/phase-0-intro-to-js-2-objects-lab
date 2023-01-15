d3174847ad851fb18633bb453ddf844629dd4adb
dismas <afubwa@gmail.com>
2023-01-15 11:58:54 +0400

    Javascript Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+let employee = {name: "Sam" ,streetAddress:"11 Broadway"};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    let employeeCopy = {...employee };
+        employeeCopy[key] = value;
+            return employeeCopy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    let employeeCopy = { ...employee };
+    delete employeeCopy[key];
+    return employeeCopy;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
